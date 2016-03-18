795a788fe54b5822e7c0ed3ad17f54eacfa3a446
yugo4k <yugo4k@gmail.com>
2016-03-17 22:10:42 -0300

    statistics of disk speeds working

--- convolutions.py
+++ convolutions.py
@@ -61,6 +61,8 @@ def convolutions(sidelength=100, n_threads=1, n_convolutions=1):
     for proc in list_proc:
         proc.join()
 
+    list_writeMBps = []
+    list_readMBps = []
     for key in sorted(return_dict.keys()):
         a_fft = return_dict[key]
         print(key, a_fft.dtype, a_fft.shape)
@@ -68,14 +70,20 @@ def convolutions(sidelength=100, n_threads=1, n_convolutions=1):
         now = time.time()
         a_fft.tofile('/tmp/a_fft_%d_%d.dat' % (key[0], key[1]))
         elapsed = time.time() - now
-        print('write: %dMB/s' % int(np.round((a_fft.size * 128 / (1024 * 1024 * 8)) / elapsed)))
+        writeMBps = int(np.round((a_fft.size * 128 / (1024 * 1024 * 8)) / elapsed))
+        list_writeMBps.append(writeMBps)
+        print('write: %dMB/s' % writeMBps)
 
         now = time.time()
         a_fft = np.fromfile('/tmp/a_fft_%d_%d.dat' % (key[0], key[1]), dtype=np.complex)
         elapsed = time.time() - now
-        print('read: %dMB/s' % int(np.round((a_fft.size * 128 / (1024 * 1024 * 8)) / elapsed)))
+        readMBps = int(np.round((a_fft.size * 128 / (1024 * 1024 * 8)) / elapsed))
+        list_readMBps.append(readMBps)
+        print('read: %dMB/s' % readMBps)
 
         print()
+
+    return np.array(list_writeMBps), np.array(list_readMBps)
 
     # if 0 < n_convolutions:
     #     a_img = np.absolute(a_img)
@@ -118,7 +126,14 @@ def main():
     #     prof.print_stats()
 
     # prof = lp.LineProfiler()
-    convolutions(sidelength=250, n_threads=4, n_convolutions=1)
+    a_writeMBps, a_readMBps = convolutions(sidelength=150, n_threads=2, n_convolutions=5)
+
+    print('max_write:', a_writeMBps.max())
+    print('avg_write:', a_writeMBps.mean())
+    print('std_write:', a_writeMBps.std())
+    print('max_read:', a_readMBps.max())
+    print('avg_read:', a_readMBps.mean())
+    print('std_read:', a_readMBps.std())
 
 
 if __name__ == '__main__':
